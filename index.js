1be609bbf435f85ad601ed9d723c46de2fcb5103
Julia <Juliapretzlaff@gmail.com>
2019-04-21 09:42:53 -0700

    used case insentive method to check for duplicates, added comments

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function hashCities(cities) {
     for (let i = 0; i < sortedCity.length; i++) {
       hashedCity += sortedCity.charCodeAt(i);
     }
+    // insert original city name at proper hashed key
     if (!cityObject[hashedCity]) {
       cityObject[hashedCity] = [city.name];
     } else {
@@ -25,14 +26,19 @@ function hashCities(cities) {
 
 function removeDuplicates(cityObject) {
   let finalRefreshedArray = [];
-  // filter duplicate city names from each key's array into 'refreshedArray'
+  // filter duplicate city names (case insensitve) from each key's array into 'refreshedArray'
+  // to ensure the following string is not printed: 'Homer, Homer, Homer, Homer'
   for (var key in cityObject) {
     if (cityObject[key].length > 1) {
       let refreshedArray = [];
       for (let i = 0; i < cityObject[key].length; i++) {
-        if (!refreshedArray.includes(cityObject[key][i]))
-          refreshedArray.push(cityObject[key][i]);
+        let cityIndex = refreshedArray.findIndex(
+          existingItem =>
+            cityObject[key][i].toLowerCase() === existingItem.toLowerCase()
+        );
+        if (cityIndex === -1) refreshedArray.push(cityObject[key][i]);
         // filter duplicate anagram sets into 'finalRefreshedArray'
+        // to ensure two strings of 'Homer, Rhome' are not printed
         if (
           refreshedArray.length > 1 &&
           !finalRefreshedArray.includes(refreshedArray)
@@ -45,16 +51,16 @@ function removeDuplicates(cityObject) {
   return finalRefreshedArray;
 }
 
-// print anagram sets to console
-// finalRefreshedArray is array of arrays/sets of anagrams
+// format string and print anagram sets to console
+// finalRefreshedArray is array of arrays/sets of anagrams with all duplicates removed
 function printStrings(finalRefreshedArray) {
   for (let i = 0; i < finalRefreshedArray.length; i++) {
     let stringToDisplay = '';
-    for (let k = 0; k < finalRefreshedArray[i].length; k++) {
-      if (k === finalRefreshedArray[i].length - 1) {
-        stringToDisplay += `${finalRefreshedArray[i][k]}`;
+    for (let j = 0; j < finalRefreshedArray[i].length; j++) {
+      if (j === finalRefreshedArray[i].length - 1) {
+        stringToDisplay += `${finalRefreshedArray[i][j]}`;
       } else {
-        stringToDisplay += `${finalRefreshedArray[i][k]}, `;
+        stringToDisplay += `${finalRefreshedArray[i][j]}, `;
       }
     }
     console.log(stringToDisplay);
